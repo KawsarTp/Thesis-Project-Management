b791fd7f840825e56ef03a953de9bc8ea37ddc08
KawsarTp <khossain227@gmail.com>
2019-04-17 15:41:30 +0600

    17-04-2019

--- resources/views/frontend/student/finder.blade.php
+++ resources/views/frontend/student/finder.blade.php
@@ -1,12 +1,15 @@
 @extends('frontend.student.master')
 @section('content')
 <div class="container mt-4">
-<ul class="list-group">
+    <div class="row">
+<ul class="list-group col-md-6">
    
       <ul class="breadcrumb">
         <li class="breadcrumb-item " aria-current="page">{{$repo}}/{{$path}}</li>
     </ul>
-
+    @foreach($contributors as $contributor)
+    <a href="/git/contributor" class="btn btn-primary">Contributors In this Project<span class="badge badge-light">{{$contributor['contributions']}}</span></a>
+    @endforeach
     @foreach($items as $item)
         <li class="list-group-item ">
             @if(isset($item['type']) && $item['type'] == 'file')
@@ -19,5 +22,12 @@
         </li>
     @endforeach
 </ul>
+<div class="col-md-6">
+    @foreach($contributors as $contributor)
+        <h3>List Of the Contributors</h3>
+        <p style="color: red;">Name of the Contributor:- {{$contributor['login']}}</p>
+    @endforeach
+</div>
+</div>
 </div>
 @endsection
